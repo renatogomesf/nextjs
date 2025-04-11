a86d6e92fdb438d0a013a209fc6d8f4f1386c313
renatogomesf <renato_gfe@hotmail.com>
2025-04-11 19:59:47 -0300

    aula 07

---
+++ src/app/aula06e07/products/page.tsx
@@ -0,0 +1,12 @@
+// todo arquivo dentro de app o next interpreta como página.
+
+export default function Products() {
+  return (
+    <div>
+      <h1>products aula 04</h1>
+      <p>produto 01</p>
+      <p>produto 02</p>
+      <p>produto 03</p>
+    </div>
+  );
+}
